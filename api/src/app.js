6ad00866eb6b7f9d4a30cac6e48cc5119cac1491
ility.gergo <ility.gergo@innomedio.hu>
2021-01-07 21:32:47 +0100

    ESLint fixes and rules

--- api/src/app.js
+++ api/src/app.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const db = require('./models/db')
+const express = require('express');
+const db = require('./models/db');
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 app.use(require('./routes/lobby'));
 
 app.listen(port, (err) => {
-    if (err) {
-        return console.error(err)
-    }
+  if (err) {
+    return console.error(err);
+  }
 
-    db.connect()
-        .then( () => {
-            return console.log(`Server is listening on ${port}`)
-        })
-})
+  db.connect()
+    .then(() => console.log(`Server is listening on ${port}`));
+
+  return true;
+});
--- api/src/routes/lobby.js
+++ api/src/routes/lobby.js
@@ -1,16 +1,17 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send('Home page');
+router.get('/', (req, res) => {
+  res.send('Home page');
 });
 
-router.get('/create', function(req, res) {
-    res.send('Create lobby');
+router.get('/create', (req, res) => {
+  res.send('Create lobby');
 });
 
-router.get('/join', function(req, res) {
-    res.send('Join lobby');
+router.get('/join', (req, res) => {
+  res.send('Join lobby');
 });
 
 module.exports = router;
