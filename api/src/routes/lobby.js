1e5a9a1c39bd6f16dcbaad75afecf27ec49c6fa3
ility.gergo <ility.gergo@innomedio.hu>
2021-01-10 13:03:23 +0100

    README fix

--- api/src/routes/lobby.js
+++ api/src/routes/lobby.js
@@ -1,17 +1,19 @@
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+router.get('/create', (req, res) => {
+  const token = jwt.sign({}, process.env.JWT_KEY, { expiresIn: '4h' });
+  res.json({token: token});
+});
+
+router.get('/:token', (req, res) => {
+  const token = req.params.token;
+  res.send(jwt.verify(token, process.env.JWT_KEY));
+});
 
 router.get('/', (req, res) => {
   res.send('Home page');
-});
-
-router.get('/create', (req, res) => {
-  res.send('Create lobby');
-});
-
-router.get('/join', (req, res) => {
-  res.send('Join lobby');
 });
 
 module.exports = router;
